df2be342b92b2aced0b401f527660c0cfa764ca1
skoya76 <kouyan120706@gmail.com>
2026-04-12 04:32:54 +0000

    dynatune: add interval feedback to AliveReply
    Carry follower heartbeat-interval feedback in AliveReply
    while keeping the old constructor available.
    
    Update AliveReplyTest to cover the new field end to end.

--- src/lsr/paxos/messages/AliveReply.java
+++ src/lsr/paxos/messages/AliveReply.java
@@ -11,24 +11,36 @@ public class AliveReply extends Message {
     private static final long serialVersionUID = 1L;
 
     private final long heartbeatId;
+    private final long heartbeatInterval;
 
     public AliveReply(int view, long heartbeatId) {
+        this(view, heartbeatId, -1L);
+    }
+
+    public AliveReply(int view, long heartbeatId, long heartbeatInterval) {
         super(view);
         this.heartbeatId = heartbeatId;
+        this.heartbeatInterval = heartbeatInterval;
     }
 
     public AliveReply(DataInputStream input) throws IOException {
         super(input);
         heartbeatId = input.readLong();
+        heartbeatInterval = input.readLong();
     }
 
     public AliveReply(ByteBuffer bb) {
         super(bb);
         heartbeatId = bb.getLong();
+        heartbeatInterval = bb.getLong();
     }
 
     public long getHeartbeatId() {
         return heartbeatId;
+    }
+
+    public long getHeartbeatInterval() {
+        return heartbeatInterval;
     }
 
     @Override
@@ -38,16 +50,18 @@ public class AliveReply extends Message {
 
     @Override
     public int byteSize() {
-        return super.byteSize() + 8;
+        return super.byteSize() + 8 + 8;
     }
 
     @Override
     protected void write(ByteBuffer bb) {
         bb.putLong(heartbeatId);
+        bb.putLong(heartbeatInterval);
     }
 
     @Override
     public String toString() {
-        return "ALIVE_REPLY (" + super.toString() + ", heartbeatId: " + heartbeatId + ")";
+        return "ALIVE_REPLY (" + super.toString() + ", heartbeatId: " + heartbeatId +
+               ", heartbeatInterval: " + heartbeatInterval + ")";
     }
 }
