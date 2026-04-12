3fdf70fa402f3c9fdd57bfed414b6f8656d9960f
skoya76 <kouyan120706@gmail.com>
2026-04-12 11:03:31 +0000

    dynatune: use heartbeat sent time for RTT
    Compute RTT using the leader timestamp carried in heartbeats so\nno per-heartbeat send-time map is needed, and record heartbeat IDs\nin sorted order while ignoring duplicates.

--- test/lsr/paxos/ActiveFailureDetectorTest.java
+++ test/lsr/paxos/ActiveFailureDetectorTest.java
@@ -110,17 +110,12 @@ public class ActiveFailureDetectorTest {
     @Test
     public void shouldMeasureRttWhenLeaderReceivesAliveReply() throws Exception {
         long heartbeatId = 42L;
-        long sentTs = ActiveFailureDetector.getTime() - 30;
         int followerId = 1;
 
-        Method trackHeartbeat = ActiveFailureDetector.class.getDeclaredMethod(
-                "trackHeartbeatSendTime", int.class, long.class, long.class);
-        trackHeartbeat.setAccessible(true);
-        trackHeartbeat.invoke(failureDetector, followerId, heartbeatId, sentTs);
-
         int suggestedHeartbeatInterval = 220;
-        invokeOnMessageReceived(failureDetector,
-                new AliveReply(0, heartbeatId, suggestedHeartbeatInterval), 1);
+        AliveReply reply = new AliveReply(0, heartbeatId, suggestedHeartbeatInterval);
+        reply.setSentTime(ActiveFailureDetector.getTime() - 30);
+        invokeOnMessageReceived(failureDetector, reply, 1);
 
         long rtt = failureDetector.getLastRttForReplica(1);
         long oneWayDelay = failureDetector.getLastOneWayDelayForReplica(1);
@@ -133,24 +128,19 @@ public class ActiveFailureDetectorTest {
     @Test
     public void shouldIgnoreOutOfRangeHeartbeatIntervalFeedback() throws Exception {
         long heartbeatId = 100L;
-        long sentTs = ActiveFailureDetector.getTime() - 30;
         int followerId = 1;
 
-        Method trackHeartbeat = ActiveFailureDetector.class.getDeclaredMethod(
-                "trackHeartbeatSendTime", int.class, long.class, long.class);
-        trackHeartbeat.setAccessible(true);
-        trackHeartbeat.invoke(failureDetector, followerId, heartbeatId, sentTs);
-
         int validInterval = 220;
-        invokeOnMessageReceived(failureDetector,
-                new AliveReply(0, heartbeatId, validInterval), followerId);
+        AliveReply reply = new AliveReply(0, heartbeatId, validInterval);
+        reply.setSentTime(ActiveFailureDetector.getTime() - 30);
+        invokeOnMessageReceived(failureDetector, reply, followerId);
         assertEquals(validInterval, failureDetector.getPerFollowerSendTimeout(followerId));
 
         long invalidHeartbeatId = 101L;
-        trackHeartbeat.invoke(failureDetector, followerId, invalidHeartbeatId,
-                ActiveFailureDetector.getTime() - 30);
-        invokeOnMessageReceived(failureDetector,
-                new AliveReply(0, invalidHeartbeatId, (long) Integer.MAX_VALUE + 1), followerId);
+        AliveReply invalidReply = new AliveReply(0, invalidHeartbeatId,
+                (long) Integer.MAX_VALUE + 1);
+        invalidReply.setSentTime(ActiveFailureDetector.getTime() - 30);
+        invokeOnMessageReceived(failureDetector, invalidReply, followerId);
 
         assertEquals(validInterval, failureDetector.getPerFollowerSendTimeout(followerId));
     }
@@ -201,7 +191,7 @@ public class ActiveFailureDetectorTest {
         invokeOnMessageReceived(failureDetector, new Alive(view, 10, 4, 12, 100), leader);
         invokeOnMessageReceived(failureDetector, new Alive(view, 10, 6, 14, 100), leader);
 
-        assertEquals(2, failureDetector.getObservedHeartbeatIdCount());
+        assertEquals(3, failureDetector.getObservedHeartbeatIdCount());
     }
 
     @Test
