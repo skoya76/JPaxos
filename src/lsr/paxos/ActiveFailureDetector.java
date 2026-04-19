91572f1a87c34892821e614b18a08146b63a31af
Kohya Shiozaki <kouyan120706@gmail.com>
2026-04-19 19:50:40 +0900

    dynatune: reset tuning state on view change
    On every view change reset the follower observation windows
    (observedRtts, observedHeartbeatIds, lastComputedEt,
    lastSuggestedHeartbeatInterval, followerTuningStarted) and the leader
    state (RTT map, per-follower send timeouts and schedules, heartbeat ID
    counters). Also restore suspectTimeout to the configured default so
    a freshly elected leader is not judged against stale RTT estimates.
    
    Rename the misspelled viewCahngeListener field to viewChangeListener.

--- src/lsr/paxos/ActiveFailureDetector.java
+++ src/lsr/paxos/ActiveFailureDetector.java
@@ -89,7 +89,7 @@ final public class ActiveFailureDetector implements Runnable, FailureDetector {
         thread = new Thread(this, "FailureDetector");
         thread.setDaemon(true);
         innerListener = new InnerMessageHandler();
-        storage.addViewChangeListener(viewCahngeListener);
+        storage.addViewChangeListener(viewChangeListener);
     }
 
     public int getDefaultSuspectTimeout() {
@@ -212,13 +212,15 @@ final public class ActiveFailureDetector implements Runnable, FailureDetector {
      *
      * @param newLeader - process id of the new leader
      */
-    protected Storage.ViewChangeListener viewCahngeListener = new Storage.ViewChangeListener() {
+    protected Storage.ViewChangeListener viewChangeListener = new Storage.ViewChangeListener() {
 
         public void viewChanged(int newView, int newLeader) {
             synchronized (ActiveFailureDetector.this) {
                 logger.debug("FD has been informed about view {}", newView);
                 view = newView;
                 lastHeartbeatRcvdTS = getTime();
+                suspectTimeout = defaultSuspectTimeout;
+                resetFollowerObservations();
                 resetLeaderObservations();
                 ActiveFailureDetector.this.notifyAll();
             }
